eb5756c8c570721149f1b720ba66ed03b474362b
Siddique-WD <mdsiddiqq101@gmail.com>
2025-07-15 09:47:55 +0530

    Updated All files

---
+++ .vscode/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['poppins', 'sans-serif'],
+      },
+      fontWeight: {
+        customWeight: '<weight>', // Replace <weight> with the desired font weight
+      },
+    },
+  },
+  plugins: [],
+}
